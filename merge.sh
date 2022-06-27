cd4c460c52a6cd4798a25e66f47dbe28c5b21b65
DonizeteVida <donizetevida1998@outlook.com>
2022-06-27 03:55:36 -0300

    Things are not working as expected. At least it compiled successfully

--- merge.sh
+++ merge.sh
@@ -9,7 +9,16 @@ name=$input
 
 echo $name
 
-bison -d -o bison_$name.c bison/$name.y
+bison -d -v -o bison_$name.c bison/$name.y
 flex -o flex_$name.c flex/$name.l
 
-cc -o $name bison_$name.c flex_$name.c
+cc -o $name -lm bison_$name.c flex_$name.c
+
+./$name
+
+rm bison_$name.c
+rm bison_$name.h
+rm bison_$name.output
+
+rm flex_$name.c
+rm $name
